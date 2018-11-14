88fcffd08ff9a16dc543e972324490e0cba71364
ron159 <zlronron159@gmail.com>
2018-11-14 23:02:06 +0800

    IMDB

--- IMDB.py
+++ IMDB.py
@@ -10,7 +10,7 @@ from keras import metrics
 
 def vectorize_sequences(sequences,dimension=10000):
     results=np.zeros((len(sequences),dimension))
-    for i sequence in enumerate(sequences):
+    for i,sequence in enumerate(sequences):
         results[i,sequence]=1.
     return results
  x_trian=vectorize_sequences(train_data)
