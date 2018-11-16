98b4938f0369b3c9376fd5bc56197e99ca52382d
ron159 <zlronron159@gmail.com>
2018-11-16 21:22:16 +0800

    bug#2

--- convnet.py
+++ convnet.py
@@ -3,15 +3,15 @@ from keras.utils import to_categorical
 from keras import layers
 from keras import models
 
-(trian_images,trian_labels),(test_images,test_labels)=mnist.load_data()
-trian_images=trian_images.reshape((60000,28,28,1))
-trian_images=trian_images.astype('float32')/255
+(train_images,train_labels),(test_images,test_labels)=mnist.load_data()
+train_images=train_images.reshape((60000,28,28,1))
+train_images=train_images.astype('float32')/255
 
 test_images=test_images.reshape((10000,28,28,1))
 test_images=test_images.astype('float32')/255
 
-trian_labels=to_categorical(trian_images)
-test_labels=to_categorical(trian_labels)
+train_labels=to_categorical(train_images)
+test_labels=to_categorical(train_labels)
 
 model=models.Sequential()
 model.add(layers.Conv2D(32,(3,3),activation='relu',input_shape=(28,28,1)))
@@ -24,4 +24,7 @@ model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10, activation='softmax')) 
 
 model.compile(optimizer='rmsprop',loss='categorical_crossentropy',metrics=['accuracy'])
-model.fit(test_images,trian_labels,epochs=5,batch_size=64)
+model.fit(train_images,train_labels,epochs=5,batch_size=64)
+
+test_loss,test_acc=model.evaluate(test_images,test_labels)
+print(test_acc)
